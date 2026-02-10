585cf355ae3e75c6a397df436daee363c33e2306
Antoinesrvt <antoine.srvt@gmail.com>
2026-02-10 17:06:24 +0100

    fix: type definition on events

--- packages/api/src/routers/api-keys.ts
+++ packages/api/src/routers/api-keys.ts
@@ -8,9 +8,10 @@
  */
 
 import { router, protectedProcedure } from "../trpc.js";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { API_KEY_SCOPES } from "@synap/database/schema";
-import { db, eq, apiKeys } from "@synap/database";
+import { db, eq, and, apiKeys, workspaceMembers } from "@synap/database";
 import { emitRequestEvent } from "../utils/emit-event.js";
 import { randomUUID, randomBytes } from "crypto";
 
